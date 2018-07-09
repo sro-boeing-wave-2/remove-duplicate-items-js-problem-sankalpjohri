a53c951302353a8d7e5971358e16d4dee05fc973
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-09 13:40:27 +0530

    Fixed lint issues

--- index.js
+++ index.js
@@ -3,6 +3,17 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => [{}];
+const removeDuplicateItems = (items) => {
+  let i = 0;
+  const itemMap = new Map();
+  const processedArray = [];
+  for (i = 0; i < items.length; i += 1) {
+    if (!itemMap.has(items[i])) {
+      itemMap.set(items[i], 1);
+      processedArray.push(items[i]);
+    }
+  }
+  return processedArray;
+};
 
 module.exports = removeDuplicateItems;
