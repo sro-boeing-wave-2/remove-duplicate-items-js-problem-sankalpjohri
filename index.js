6c8b4ba473c87180c26a8677181847bf20155a26
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-10 11:33:28 +0530

    Changes according to review comments

--- index.js
+++ index.js
@@ -3,21 +3,7 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {
-  let i = 0;
-  const itemMap = new Map();
-  const processedArray = [];
 
-  /* Iterate over the elements of the arary and check if the entry exists in the map.
-   * If the element exists, ignore it otherwise add it to the map and the processedArray.
-   */
-  for (i = 0; i < items.length; i += 1) {
-    if (!itemMap.has(items[i])) {
-      itemMap.set(items[i], 1);
-      processedArray.push(items[i]);
-    }
-  }
-  return processedArray;
-};
+const removeDuplicateItems = items => Array.from(new Set(items).values());
 
 module.exports = removeDuplicateItems;
