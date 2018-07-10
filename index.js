190542e01e50c7c37f5930c1fc9e382229abcd4b
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-10 10:04:05 +0530

    Added comments

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const removeDuplicateItems = (items) => {
   let i = 0;
   const itemMap = new Map();
   const processedArray = [];
+
+  /* Iterate over the elements of the arary and check if the entry exists in the map.
+   * If the element exists, ignore it otherwise add it to the map and the processedArray.
+   */
   for (i = 0; i < items.length; i += 1) {
     if (!itemMap.has(items[i])) {
       itemMap.set(items[i], 1);
